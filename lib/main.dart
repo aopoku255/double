aebf47d6cca44600c62b74f5b94c2e5395558546
aopoku255 <aopoku255@gmail.com>
2026-01-31 10:18:48 +0000

    Removed firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:doubles/firebase_options.dart';
-import 'package:doubles/src/api/firebase_api.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:doubles/firebase_options.dart';
+// import 'package:doubles/src/api/firebase_api.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseApi().initNotifications();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseApi().initNotifications();
   runApp(const App());
 }
